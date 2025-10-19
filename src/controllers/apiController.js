d39c44fb509c4df5865f92c89152d04b67559618
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-19 15:05:34 +0300

    Update apiController.js

--- src/controllers/apiController.js
+++ src/controllers/apiController.js
@@ -1,5 +1,13 @@
-export function getHome(req, res) {
-  res.status(200).json("Home Page");
+import User from "../models/User.js";
+
+export async function getHome(req, res) {
+  try {
+    const users = await User.find();
+    res.status(200).json(users);
+  } catch (error) {
+    console.error("Error in getHome method", error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 }
 
 export function getAbout(req, res) {
