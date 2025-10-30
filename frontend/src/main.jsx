74cd691ccb85997245c7874499e0c60ec86a0c25
iHussamAlshayeb <hussamalshayeb01@gmail.com>
2025-10-30 13:24:36 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { BrowserRouter } from 'react-router'
+import App from './App.jsx'
 import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
