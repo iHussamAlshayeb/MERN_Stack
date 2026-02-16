869a0a836f4747d324c83cbb21f00e1e1704f602
Hussam Alshayeb <hussamalshayeb01@gmail.com>
2026-02-16 21:32:45 +0300

    MONGODB

---
+++ 13_MONGODB/routes/api/employees.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const {
+  getAllEmployees,
+  createEmployee,
+  deleteEmployee,
+  updateEmployee,
+  getEmployee,
+} = require("../../controllers/employeesController");
+
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../middleware/verifyRoles");
+
+router
+  .route("/")
+  .get(getAllEmployees)
+  .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), createEmployee)
+  .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), updateEmployee)
+  .delete(verifyRoles(ROLES_LIST.Admin), deleteEmployee);
+
+router.route("/:id").get(getEmployee);
+
+module.exports = router;
