e4cafde3b7b476df03821fc61ec4652bda114e7c
Hussam Alshayeb <hussamalshayeb01@gmail.com>
2026-02-16 20:15:07 +0300

    Roles

--- 12_Roles/routes/api/employees.js
+++ 12_Roles/routes/api/employees.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const router = express.Router();
 const {
   getAllEmployees,
   createEmployee,
@@ -7,14 +8,15 @@ const {
   getEmployee,
 } = require("../../controllers/employeesController");
 
-const router = express.Router();
+const ROLES_LIST = require("../../config/roles_list");
+const verifyRoles = require("../../middleware/verifyRoles");
 
 router
   .route("/")
   .get(getAllEmployees)
-  .post(createEmployee)
-  .put(updateEmployee)
-  .delete(deleteEmployee);
+  .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), createEmployee)
+  .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), updateEmployee)
+  .delete(verifyRoles(ROLES_LIST.Admin), deleteEmployee);
 
 router.route("/:id").get(getEmployee);
 
