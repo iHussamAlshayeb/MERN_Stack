48b96ea907cc922586de18000f3a5a4ef242e171
Hussam Alshayeb <hussamalshayeb01@gmail.com>
2026-02-16 11:52:14 +0300

    Roles

---
+++ 12_Roles/config/allowedOrigins.js
@@ -0,0 +1,8 @@
+//cors - Cross Origin Resource Sharing
+const allowedOrigins = [
+  "https://ihussam.dev",
+  "http://localhost:5173",
+  "http://localhost:3500",
+];
+
+module.exports = allowedOrigins;
